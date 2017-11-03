83b6c78461e9d24d343e0b854c0517d8ecd61568
William H Cass <528945@hull.ac.uk>
2017-11-03 16:59:13 +0000

    Committing initial files

---
+++ SailAway/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SailAway
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            using (var game = new SailAway())
+                game.Run();
+        }
+    }
+}
