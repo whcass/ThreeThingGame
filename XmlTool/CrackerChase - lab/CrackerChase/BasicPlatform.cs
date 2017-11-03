c49efb96fdfdb29b33513c1e84ec84e93f7dd18b
James Williams <531989@hull.ac.uk>
2017-11-03 17:39:46 +0000

    Copied XmlTool stuff into separate project to avoid colisions.
    Signed-off-by: James Williams <531989@hull.ac.uk>

---
+++ XmlTool/CrackerChase - lab/CrackerChase/BasicPlatform.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CrackerChase
+{
+    class BasicPlatform : Sprite
+    {
+        protected Color mColor;
+
+        protected string mName;
+
+        public BasicPlatform(int pScreenWidth, int pScreenHeight, Texture2D pTexture, int pDrawWidth, int pDrawHeight, float pResetX, float pResetY, Color pColor, string pName) : base(pScreenWidth,pScreenHeight,pTexture,pDrawWidth,pResetX,pResetY)
+        {
+            mName = pName;
+            //mRectangle = new Rectangle(0,0,pWidth,pHeight);
+            mColor = pColor;
+            GarbageThatRuinsMyPlatformGenerationTool(pTexture, pDrawWidth, pDrawHeight);
+            Reset();
+        }
+        public override void GarbageThatRuinsMyPlatformGenerationTool(Texture2D pTexture, int pDrawWidth, int pDrawHeight)
+        {
+            rectangle = new Rectangle(0, 0, pDrawWidth, pDrawHeight);
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            rectangle.X = (int)Math.Round(xPosition);
+            rectangle.Y = (int)Math.Round(yPosition);
+            spriteBatch.Draw(texture, rectangle, mColor);
+        }
+    }
+}
---
+++ XmlTool/CrackerChase - lab/CrackerChase/Target-2017-11-03_05-46-07-363.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackerChase
+{
+    class Target : Sprite
+    {
+
+        static Random rand = new Random();
+        override public void Reset()
+        {
+            int x = rand.Next(0, mScreenWidth - rectangle.Width);
+            int y = rand.Next(0, screenHeight - rectangle.Height);
+            SetPosition(x, y);
+        }
+
+        public Target(int inScreenWidth, int inScreenHeight, Texture2D inSpriteTexture, int inDrawWidth, float inResetX, float inResetY) :
+            base(inScreenWidth, inScreenHeight, inSpriteTexture, inDrawWidth, inResetX, inResetY)
+        {
+        }
+    }
+}
