36a97105f1a2789d92cda0b9ab7e4922f18004c0
James Williams <531989@hull.ac.uk>
2017-11-03 19:05:12 +0000

    idk at this point
    Signed-off-by: James Williams <531989@hull.ac.uk>

--- SailAway/XmlTool.cs
+++ SailAway/XmlTool.cs
@@ -12,7 +12,6 @@ namespace SailAway
 
     class XmlTool
     {
-        protected 
         protected XmlTool()
         {
 
@@ -21,16 +20,18 @@ namespace SailAway
         {
             XmlNode LevelNode = XmlFile.FirstChild.NextSibling;
 
-            foreach (XmlNode lBasicPlatform in LevelNode.SelectSingleNode("BasicPlatforms").ChildNodes)
+            foreach (XmlNode lPlatform in LevelNode.SelectSingleNode("BasicPlatforms").ChildNodes)
             {
-                XmlNode ColorNode = lBasicPlatform.SelectSingleNode("ColorRGBA");//I think it's more effecient to store this location and not write it out four times in the line below but idk.
-                Color mBasicPlatformColor = new Color(new Vector4(ChildNodeIntFromParent(ColorNode, "Red"), ChildNodeIntFromParent(ColorNode, "Green"), ChildNodeIntFromParent(ColorNode, "Blue"), ChildNodeIntFromParent(ColorNode, "Alpha")));
-
-                XmlNode CoordinatesNode = lBasicPlatform.SelectSingleNode("Coordinates");//I think it's more effecient to store this location and not write it out four times in the line below but idk.
-                Platform platform = new Platform();
-                Platform basicBasicPlatform = new Platform( Pixel, Math.Abs(ChildNodeIntFromParent(CoordinatesNode, "x1") - ChildNodeIntFromParent(CoordinatesNode, "x2")), Math.Abs(ChildNodeIntFromParent(CoordinatesNode, "y1") - ChildNodeIntFromParent(CoordinatesNode, "y2")), ChildNodeIntFromParent(CoordinatesNode, "x1"), ChildNodeIntFromParent(CoordinatesNode, "x2"), mBasicPlatformColor, ChildNodeStringFromParent(lBasicPlatform, "Name"));
-                Platform basicBasicPlatform2 = new Platform( Pixel, 300, 300, 1, 1, Color.HotPink, "cock");
+                SailAway sailaway = new SailAway(XmlFile);
+                //XmlNode ColorNode = lPlatform.SelectSingleNode("ColorRGBA");//I think it's more effecient to store this location and not write it out four times in the line below but idk.
+                //Color mBasicPlatformColor = new Color(new Vector4(ChildNodeIntFromParent(ColorNode, "Red"), ChildNodeIntFromParent(ColorNode, "Green"), ChildNodeIntFromParent(ColorNode, "Blue"), ChildNodeIntFromParent(ColorNode, "Alpha")));
+                XmlNode CoordinatesNode = lPlatform.SelectSingleNode("Coordinates");//I think it's more effecient to store this location and not write it out four times in the line below but idk.
+                Texture2D mTexture = sailaway.GenerateRedBox(32, 32);
+                Platform platform = new Platform(mTexture, 32, 32, ChildNodeIntFromParent(CoordinatesNode, "Length"));
+                //Platform basicBasicPlatform = new Platform( Pixel, Math.Abs(lp - ChildNodeIntFromParent(CoordinatesNode, "x2")), Math.Abs(ChildNodeIntFromParent(CoordinatesNode, "y1") - ChildNodeIntFromParent(CoordinatesNode, "y2")), ChildNodeIntFromParent(CoordinatesNode, "x1"), ChildNodeIntFromParent(CoordinatesNode, "x2"), mBasicPlatformColor, ChildNodeStringFromParent(lPlatform, "Name"));
+                //Platform basicBasicPlatform2 = new Platform( Pixel, 300, 300, 1, 1, Color.HotPink, "cock");
             }
+
         }
 
 
